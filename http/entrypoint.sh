17971e830979949825d57a95148d6b9fc256ec11
huangjun <huangjun@zhiihuianxin.com>
2018-05-30 16:04:25 +0800

    修改成upstream模式

--- http/entrypoint.sh
+++ http/entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 cp /etc/nginx/conf.d/service.conf.template /etc/nginx/conf.d/service.conf \
-&& sed -i 's,%{HTTP_URL}%,'"${HTTP_URL}"',g' /etc/nginx/conf.d/service.conf \
+&& sed -i 's,%{UPSTREAM_SERVICES}%,'"${UPSTREAM_SERVICES}"',g' /etc/nginx/conf.d/service.conf \
 && nginx -g "daemon off;"
