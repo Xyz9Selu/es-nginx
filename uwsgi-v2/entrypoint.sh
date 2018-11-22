cb32486ffa2b944f89548cbbfe97b69f44779f7a
John Huang <huangjun.emc2@gmail.com>
2018-11-22 17:07:11 +0800

    添加uwsgi-v2

---
+++ uwsgi-v2/entrypoint.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+cp /etc/nginx/conf.d/service.conf.template /etc/nginx/conf.d/service.conf \
+&& sed -i 's,%{UPSTREAM_UWSGI_SERVICES}%,'"${UPSTREAM_UWSGI_SERVICES}"',g' /etc/nginx/conf.d/service.conf \
+&& nginx -g "daemon off;"
