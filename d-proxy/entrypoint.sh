8fd199987c95d7ec0f59f4da2a6ccb587aa8c749
John Huang <huangjun.emc2@gmail.com>
2018-12-29 12:43:14 +0800

    update d-proxy

--- d-proxy/entrypoint.sh
+++ d-proxy/entrypoint.sh
@@ -2,5 +2,6 @@
 
 cp /etc/nginx/conf.d/service.conf.template /etc/nginx/conf.d/service.conf \
 && sed -i 's,%{TARGET_HOST}%,'"${TARGET_HOST}"',g' /etc/nginx/conf.d/service.conf \
+&& sed -i 's,%{TARGET_URL}%,'"${TARGET_URL}"',g' /etc/nginx/conf.d/service.conf \
 && cat /etc/nginx/conf.d/service.conf \
 && nginx -g "daemon off;"
