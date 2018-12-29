d873f18782812cca53b7b36256a0a837665b1977
John Huang <huangjun.emc2@gmail.com>
2018-12-29 12:21:54 +0800

    + d-proxy

---
+++ d-proxy/entrypoint.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+cp /etc/nginx/conf.d/service.conf.template /etc/nginx/conf.d/service.conf \
+&& sed -i 's,%{TARGET_HOST}%,'"${TARGET_HOST}"',g' /etc/nginx/conf.d/service.conf \
+&& cat /etc/nginx/conf.d/service.conf \
+&& nginx -g "daemon off;"
